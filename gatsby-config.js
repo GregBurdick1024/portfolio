3aa1d7bdfc2d71dd08acc55bdd8de8de7fb459fe
Greg <gregburdick85@gmail.com>
2022-09-07 16:47:13 +0100

    full commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,28 @@
 module.exports = {
-  siteMetadata: {
-    title: `My Portfolio 2022`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+	siteMetadata: {
+		title: `My Portfolio 2022`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		'gatsby-plugin-image',
+		'gatsby-plugin-sharp',
+		`gatsby-plugin-sass`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-offline`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images/`,
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `pages`,
+				path: `${__dirname}/src/pages/`,
+			},
+		},
+	],
+};
---
+++ src/data.js
@@ -0,0 +1,89 @@
+import { nanoid } from 'nanoid';
+
+const githubURL = 'https://github.com/GregBurdick1024';
+
+// HEAD DATA
+export const headData = {
+	title: 'Greg Burdick - Web Dev',
+	lang: 'en',
+	description: 'Welcome to my portfolio',
+};
+
+// HERO DATA
+export const heroData = {
+	title: 'Hi, you have found the Porfolio of',
+	name: 'Greg Burdick',
+	subtitle: 'Web Developer',
+	cta: 'Learn more',
+};
+
+// ABOUT DATA
+export const aboutData = {
+	img: 'mango.jpg',
+	paragraphOne:
+		'I find myself at my best when I am solving problems and learning new things. From a casual curiosity in programming back in early 2018 I quickly discovered that this was the direction I wished to persue.',
+	paragraphTwo:
+		'At present I am looking for work as a junior developer, I have come a long way and hope that I can continue to develop and grow in this exciting industry.',
+	paragraphThree:
+		'When I am not spending my time writing code I love to keep fit  and keep up to date with all the developments in blockchain technology.',
+	resume: 'https://www.gregburdick.online/documents/GregBurdick-CV-2020.pdf',
+};
+
+// PROJECTS DATA
+export const projectsData = [
+	{
+		id: nanoid(),
+		img: 'mma-match-app.png',
+		title: 'MMA Match App',
+		info: 'I love mixed martial arts and wanted an app that allowed users to compare fighters.  As the title implies this app allows users to add fighters to their favorites where they can check their stats and "match" two fighters against eachother.',
+		info2: 'Tech Stack: MongoDB, NodeJS, ExpressJS, ReactJS with React-Bootstrap for styling and D3 for charts.',
+		url: 'https://mma-match-app.herokuapp.com/',
+		repo: 'https://github.com/flushy85/mma-match-app-backend',
+	},
+	{
+		id: nanoid(),
+		img: 'pomo-todo.png',
+		title: 'Pomo To-do List',
+		info: 'Combining two productivity apps into one this app allows users to manage their time more efficiently. Users are able to add tasks to a todo-list and plan their work schedule using the classic pomodoro method.',
+		info2: 'Tech Stack: ReactJS with Material-UI for styling',
+		url: 'https://pomo-todo.netlify.app/',
+		repo: `${githubURL}/pomodoro-tasks`,
+	},
+	{
+		id: nanoid(),
+		img: 'top-flicks.png',
+		title: 'Good Pics (WIP)',
+		info: 'Inspired by the website Good-Reads, this app allows users to rate their favourite films and create ranked lists of films to share.',
+		info2: 'Tech Stack: MySQL, NodeJS, ExpressJS, ReactJS with Tailwind for styling.',
+		url: '',
+		repo: `${githubURL}/topflicks`,
+	},
+];
+
+// CONTACT DATA
+export const contactData = {
+	cta: 'Would you like to work with me? Awesome!',
+	btn: "Let's Talk",
+	email: 'gregrburdick@protonmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+	networks: [
+		{
+			id: nanoid(),
+			name: 'codepen',
+			url: 'https://codepen.io/flushy85',
+		},
+		{
+			id: nanoid(),
+			name: 'github',
+			url: githubURL,
+		},
+	],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+	isEnabled: false,
+};
